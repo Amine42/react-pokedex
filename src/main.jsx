38ebf8fc7e3aef869d911bfe276bfbfc8b71329f
Amine42 <amine.bouraghda@gmail.com>
2023-05-15 15:44:29 +0200

    disable react query devtools

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ import {
   RouterProvider,
   Route,
 } from 'react-router-dom'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import Root from './routes/root'
 import ErrorPage from './routes/error-page'
 import App from './App'
@@ -30,7 +29,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
         <RouterProvider router={router} />
-        <ReactQueryDevtools />
       </ChakraProvider>
     </QueryClientProvider>
   </React.StrictMode>
