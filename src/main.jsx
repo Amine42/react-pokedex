18fefa66afc691e7387ce63633c0122c593dec47
Amine42 <amine.bouraghda@gmail.com>
2023-05-16 01:55:06 +0200

    disable react strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -25,11 +25,9 @@ const router = createBrowserRouter(
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
-        <RouterProvider router={router} />
-      </ChakraProvider>
-    </QueryClientProvider>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </QueryClientProvider>
 )
